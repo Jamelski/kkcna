6f9d0046f7e06d95dce91be3b8b70e0a8f6209ac
Jamel Lawson <lawson.jamel@yahoo.com>
2016-05-16 19:30:07 -0700

    Initial commit

---
+++ sites/all/themes/kkcnaTheme/js/scripts.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+/*jQuery( document ).ready(function($) {
+	( ".views-row" ).wrap( "<div class='col-sm-4 col-md-3'></div>" );
+});*/
+
+
+ 
+  $( document ).ready(function() {
+    $( ".view-meet-us .views-row" ).wrap( "<div class='col-sm-4 col-md-3'></div>" );
+  });
+ 
